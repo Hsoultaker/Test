544da49f0f823a4b26c439c2975de768b866c084
Hsoultaker <Hsoultaker@sina.com>
2016-02-15 21:36:20 +0800

    Add

---
+++ Test/Test.cpp
@@ -0,0 +1,15 @@
+// Test.cpp : 定义控制台应用程序的入口点。
+//
+
+#include "stdafx.h"
+#include <iostream>
+using namespace std;
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	cout<<"Hello world!"<<endl;
+ 	cout<<"And one row"<<endl;
+	cout<<"look out steph"<<endl;
+	return 0;
+}
+
